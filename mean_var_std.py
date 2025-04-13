8e133a6030e077484a3d118c1babf9a3c5cbbc62
Andrea Graziano Gitto <andreagraziano.g33personale@gmail.com>
2025-04-13 11:10:24 +0000

    end

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,23 +1,18 @@
 import numpy as np
 
-def calculate(list):
-    calculations = {"mean": [], "variance": [], "standard deviation": [], "min": [], "max": [], "sum": []}
-    error =  "List must contain nine numbers."
-    try:
-        a=list[9]
-    except  ValueError:
-        print(error)
+def calculate(input_list):
+    if len(input_list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
-    mat = np.array [[list[:3]],[list[3:6]],[list[6:9]]]
-    
-    calculations["mean"] = [np.mean(mat, axis=0).tolist(), np.mean(mat, axis=1).tolist(), np.mean(mat).tolist()]
-    calculations["variance"] = [np.var(mat, axis=0).tolist(), np.var(mat, axis=1).tolist(), np.var(mat).tolist()]
-    calculations["standard deviation"] = [np.std(mat, axis=0).tolist(), np.std(mat, axis=1).tolist(), np.std(mat).tolist()]
-    calculations["min"] = [np.min(mat, axis=0).tolist(), np.min(mat, axis=1).tolist(), np.min(mat.flatten()).tolist()]
-    calculations["max"] = [np.max(mat, axis=0).tolist(), np.max(mat, axis=1).tolist(), np.max(mat.flatten()).tolist()]
-    calculations["sum"] = [np.sum(mat, axis=0).tolist(), np.sum(mat, axis=1).tolist(), np.sum(mat.flatten()).tolist()]
-    
+    mat = np.array([input_list[:3], input_list[3:6], input_list[6:9]])
 
-
+    calculations = {
+        "mean": [np.mean(mat, axis=0).tolist(), np.mean(mat, axis=1).tolist(), np.mean(mat).tolist()],
+        "variance": [np.var(mat, axis=0).tolist(), np.var(mat, axis=1).tolist(), np.var(mat).tolist()],
+        "standard deviation": [np.std(mat, axis=0).tolist(), np.std(mat, axis=1).tolist(), np.std(mat).tolist()],
+        "min": [np.min(mat, axis=0).tolist(), np.min(mat, axis=1).tolist(), np.min(mat).tolist()],
+        "max": [np.max(mat, axis=0).tolist(), np.max(mat, axis=1).tolist(), np.max(mat).tolist()],
+        "sum": [np.sum(mat, axis=0).tolist(), np.sum(mat, axis=1).tolist(), np.sum(mat).tolist()]
+    }
 
     return calculations
